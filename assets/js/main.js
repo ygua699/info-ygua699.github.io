92ecc16ed7b2e707a1711c7e799cb766054fb27d
MeloGuan <meloguan123@gmail.com>
2021-04-12 20:42:38 +1200

    renew

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+// const htmlEl = document.getElementsByTagName('html')[0];
+const htmlEl = document.body;
+
+const toggleTheme = () => {
+    htmlEl.classList.toggle("dark-mode");
+    document.getElementById("darkModeIcron").innerHTML = '<i class="bx bxs-sun" onclick="toggleTheme1();" style="cursor: pointer;"></i>';
+}
+
+const toggleTheme1 = () => {
+    htmlEl.classList.toggle("dark-mode");
+    document.getElementById("darkModeIcron").innerHTML = '<i class="bx bxs-moon" onclick="toggleTheme();" style="cursor: pointer;"></i>';
+}
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', { delay: 400 });
+sr.reveal('.about__text', { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', { interval: 200 });
+sr.reveal('.skills__img', { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal('.work__img', { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input', { interval: 200 });
+
+
+
+
